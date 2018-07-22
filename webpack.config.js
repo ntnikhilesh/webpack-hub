63a034ce7b1ba9d1adb461eb1f0117928383a42e
ntnikhilesh <ntnikhilesh@gmail.com>
2018-07-22 22:22:56 +0530

    remove dist on every build using cleanwebpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,7 @@
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 var extractPlugin = new ExtractTextPlugin({
    filename: 'main.css'
@@ -88,6 +89,7 @@ module.exports = {
         extractPlugin,
         new HtmlWebpackPlugin({
           template: 'src/index.html'
-        })
+        }),
+        new CleanWebpackPlugin(['dist'])
     ]
 };
