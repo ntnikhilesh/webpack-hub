dfc8113427f89d1bf1c67457eff32a467e23c977
ntnikhilesh <ntnikhilesh@gmail.com>
2018-07-22 19:23:03 +0530

    Module - loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,14 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/dist'
   },
+
+  module:{
+    rules:[{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }]
+  }
 };
